fd1b9f02e745a8e2a67bb2c32a0c2e3681e40e85
Боев Антон <antonboev421@gmail.com>
2026-09-11 11:47:03 +0300

    Ввести контракт хранилища IVisitRepository в слое логики

---
+++ AttendanceLog.Logic/Visit.cs
@@ -0,0 +1,8 @@
+namespace AttendanceLog.Logic;
+
+public class Visit
+{
+    public int Id { get; set; }
+    public string Student { get; set; } = "";
+    public bool WasPresent { get; set; }
+}
--- AttendanceLog.Logic/VisitService.cs
+++ AttendanceLog.Logic/VisitService.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
 using System.Linq;
-using AttendanceLog.Data;
 
 namespace AttendanceLog.Logic;
 
 public class VisitService
 {
-    private readonly VisitRepository _repository = new();
-
-    public List<Visit> GetAbsences()
+    private readonly IVisitRepository _repository;
+    public VisitService(IVisitRepository repository)
+    {
+        _repository = repository;
+    }
+    public List<Visit> GetImportant()
     {
         return _repository.GetAll()
             .Where(item => item.WasPresent == false)
